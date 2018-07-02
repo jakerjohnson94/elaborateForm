0f5e6f69ea82dec77d2dcf4224ca5ec844bd201a
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-07-02 16:03:35 -0400

    init

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const express = require("express")
-const path = require("path")
+const express = require('express');
+const path = require('path');
 
-const app = express()
-const publicFolderPath = path.join(__dirname, "public")
+const app = express();
+const publicFolderPath = path.join(__dirname, 'public');
 
-app.use(express.json())
-app.use(express.static(publicFolderPath))
+app.use(express.json());
+app.use(express.static(publicFolderPath));
 
-const users = []
+const users = [];
 
-// add POST request listener here
+// app.get('./api/user', (req, res) => {
+//   res.status(200);
 
-app.listen(3000);
+//   res.send(users);
+// });
+
+app.post('/api/user', (req, res) => {
+  const isDuplicateUsername = false;
+  const currentUsername = req.body.username;
+  console.log();
+  users.forEach(user => {
+    if (user.username === req.body.username) isDuplicateUsername = true;
+  });
+  if (isDuplicateUsername === false) {
+    res.status(201);
+    req.body.id = Math.floor(Math.random() * 100000);
+    users.push(req.body);
+    res.send(users);
+  } else {
+    res.status(409);
+    throw new Error('username already taken');
+  }
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- public/index.js
+++ public/index.js
@@ -1,2 +1,46 @@
-const userCreateForm = document.getElementById("user-create-form")
-const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+const userCreateForm = document.getElementById('user-create-form');
+const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']");
+const userCreateEmail = userCreateForm.querySelector('#email');
+const userCreateUsername = userCreateForm.querySelector('#username');
+const userCreatePassword = userCreateForm.querySelector('#password');
+const userCreateName = userCreateForm.querySelector('#name');
+let user;
+
+function User(email, username, password, name) {
+  this.email = email;
+  this.username = username;
+  this.password = password;
+  this.name = name;
+}
+
+const clickEvent = function(event) {
+  event.preventDefault();
+
+  user = new User(
+    userCreateEmail.value,
+    userCreateUsername.value,
+    userCreatePassword.value,
+    userCreateName.value
+  );
+  console.log(user);
+
+  fetch('http://localhost:3000/api/user', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .then(data => {
+      console.log('Success:', data[0]);
+    })
+    .catch(error => console.error('Error:', error));
+};
+
+userCreateSubmitButton.addEventListener('click', clickEvent);
